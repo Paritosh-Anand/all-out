051e6bdc471f5629cfa17f8bfb37876d4cc45660
Paritosh-Anand <kakapari@gmail.com>
2016-11-22 23:49:24 +0530

    count words in camel case string

---
+++ src/main/java/string/CamelCase.java
@@ -0,0 +1,28 @@
+package string;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CamelCase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		String s = scan.next();
+		
+		int wordCount = 1;
+		Pattern pattern = Pattern.compile("[A-Z]");
+		Matcher matcher = pattern.matcher(s);
+		
+		while(matcher.find()) {
+			wordCount++;
+			//System.out.println(matcher.group());
+		}
+		
+		scan.close();
+		System.out.println(wordCount);
+
+	}
+
+}
